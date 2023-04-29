545a7c2e68eb17509a8b82a9dfa9810e416901a4
TotoXL <116646671+TotoXL@users.noreply.github.com>
2023-04-29 19:26:40 +0200

    Create TotoScript.lua

---
+++ TotoScript.lua
@@ -0,0 +1,21 @@
+local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/xHeptc/Kavo-UI-Library/main/source.lua"))()
+
+local Window = Library.CreateLib("TITLE", "DarkTheme")
+
+local Tab = Window:NewTab("Normal Scipts")
+
+local Section = Tab:NewSection("Scripts")
+
+Section:NewButton("FullBright", "Fully Bright", function()
+    local Light = game:GetService("Lighting")
+
+function dofullbright()
+Light.Ambient = Color3.new(1, 1, 1)
+Light.ColorShift_Bottom = Color3.new(1, 1, 1)
+Light.ColorShift_Top = Color3.new(1, 1, 1)
+end
+
+dofullbright()
+
+Light.LightingChanged:Connect(dofullbright)
+end)
